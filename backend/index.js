3b9481873d5c7c88d67f47b860d426871229896a
Naveen <naveenrajanm9@gmail.com>
2024-02-25 19:16:59 +0530

    nodemailer

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const RegisterData = require("./schema/login.js");
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+const nodemailer = require("nodemailer");
+const Apppassword = encodeURIComponent(process.env.MAIL_PASSWORD);
 
 mongoose.connect(URL, {
   useNewUrlParser: true,
@@ -34,14 +36,48 @@ app.post("/Login", (req, res) => {
 app.post("/register", async (req, res) => {
   const { name, password, email, phone } = req.body;
   const existingUser = await RegisterData.findOne({ name });
+  const existingEmail = await RegisterData.findOne({ email });
+  const existingNumber = await RegisterData.findOne({ phone });
 
   if (existingUser) {
-    res.json("already a user");
+    res.json("Username Already Exists");
   } else {
+    if (existingEmail || existingNumber) {
+      res.json("Phone number or email already used");
+    } else {
       RegisterData.create(req.body)
-      .then((data) => res.json(data))
-      .catch((error) => res.json(error));
+        .then((data) => res.json(data))
+        .catch((error) => res.json(error));
+    }
   }
+});
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "pipelineoffecial@gmail.com",
+    pass: Apppassword,
+  },
+});
+
+app.post("/sendpassword/:email", async (req, res) => {
+  const { email } = req.params;
+
+  const user = await RegisterData.findOne({ email });
+
+  if (user) {
+    const gotpassword = user.password;
+    const username = user.name;
+    console.log(gotpassword, username);
+
+    await transporter.sendMail({
+      to: email,
+      subject: `your password ms/mr ${username}`,
+      text: `Your password is: ${gotpassword}`,
+    });
+
+    res.json("password sent");
+  } else res.json("usernot found!!..");
 });
 
 app.listen(port, () => {
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -8,7 +8,9 @@ import "../../components/global.css";
 export const Login = () => {
   const [Input, setInput] = useState("");
   const [Password, setPassword] = useState("");
+  const [forgetemail  , setforgetemail] = useState("");
   const [Checked, setChecked] = useState(false);
+  const [forgetpassword, setforgetpassword] = useState(false);
 
   const HandleInput = (event) => {
     setInput(event.target.value);
@@ -24,51 +26,96 @@ export const Login = () => {
 
   const HandleForm = (event) => {
     event.preventDefault();
-    
+
     axios
       .post(`http://localhost:10000/Login`, { Input, password: Password })
-      .then((result) => console.log(result) )
+      .then((result) => console.log(result))
       .catch((error) => console.log(error));
   };
+
+  const ForgetPassword = () => {
+    setforgetpassword(!forgetpassword);
+  }
+
+  const passSender = (event) => {
+    event.preventDefault();
+    console.log(forgetemail);
+
+    axios
+      .post(`http://localhost:10000/sendpassword/${forgetemail}`, {})
+      .then((outcome) => console.log(outcome))
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div className="container d-flex align-items-center justify-content-center vh-100">
       <div className="col-md-6">
-        <form onSubmit={HandleForm}>
-          <div className="mb-3">
-            <CustomInput
-              _type="text"
-              _placeholder="Username"
-              _function={HandleInput}
-              _class="form-control"
-            />
-          </div>
-          <div className="mb-3">
-            <CustomInput
-              _type={Checked ? "text" : "password"}
-              _placeholder="Password"
-              _function={HandlePassword}
-              _class="form-control"
-            />
-          </div>
-          <div className="mb-3 form-check">
-            <CustomInput
-              _type="checkbox"
-              _function={Showpassword}
-              _class="form-check-input"
-            />
-            <label className="form-check-label">Show Password</label>
-          </div>
-          <p className="mt-3 myfont">
-            don't have a account? <Link to="/">register</Link>
-          </p>
-          <Commonbutton
-            _function={HandleForm}
-            _content="Login"
-            _class="btn btn-primary"
-          />
+        <form >
+          {forgetpassword ? (
+            <>
+              <h1 className="text-primary text-capitalize heading mb-3">forget password</h1>
+              <CustomInput
+                _type="email"
+                _placeholder="email.."
+                _function={(e) => {
+                  setforgetemail(e.target.value);
+                }}
+                _class="form-control mb-3"
+              />
+              <Commonbutton
+                _function={passSender}
+                _content="send"
+                _class="btn btn-primary "
+              />
+            </>
+          ) : (
+            <>
+              <div className="mb-3">
+                <CustomInput
+                  _type="text"
+                  _placeholder="Username"
+                  _function={HandleInput}
+                  _class="form-control text-capitalize"
+                />
+              </div>
+              <div className="mb-3">
+                <CustomInput
+                  _type={Checked ? "text" : "password"}
+                  _placeholder="Password"
+                  _function={HandlePassword}
+                  _class="form-control"
+                />
+              </div>
+              <div className="mb-3 form-check d-flex justify-content-between align-items-center">
+                <div>
+                  <CustomInput
+                    _type="checkbox"
+                    _function={Showpassword}
+                    _class="form-check-input"
+                  />
+                  <label className="form-check-label myfont">
+                    Show Password
+                  </label>
+                </div>
+                <label
+                  className="form-check-label myfont cursor"
+                  onClick={ForgetPassword}
+                >
+                  Forget Password?
+                </label>
+              </div>
+              <p className="mt-3 myfont">
+                don't have an account? <Link to="/">register</Link>
+              </p>
+              <Commonbutton
+                _function={HandleForm}
+                _content="Login"
+                _class="btn btn-primary"
+              />
+            </>
+          )}
         </form>
       </div>
     </div>
   );
-};
+};  
